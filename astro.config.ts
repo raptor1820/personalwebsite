2acf710866ca072d04f796e7fcd259d96a0ab01e
Ritwic <ritwic.verma.sachan@gmail.com>
2024-09-10 16:30:44 -0400

    fixed vercel analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,24 +1,29 @@
-import image from '@astrojs/image';
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
-import { defineConfig } from 'astro/config';
-import { visualizer } from 'rollup-plugin-visualizer';
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), compress()],
-  vite: {
-    plugins: [visualizer()],
-    resolve: {
-      alias: {
-        'date-fns/locale': 'date-fns/locale/index.js',
-      },
+    integrations: [tailwind(), image(), compress()],
+    vite: {
+        plugins: [visualizer()],
+        resolve: {
+            alias: {
+                "date-fns/locale": "date-fns/locale/index.js",
+            },
+        },
     },
-  },
-  redirects: {
-    '/pdf': '/',
-  },
-  experimental: {
-    redirects: true,
-  },
+    redirects: {
+        "/pdf": "/",
+    },
+    experimental: {
+        redirects: true,
+    },
+    output: "server",
+    adapter: vercel({
+        analytics: true,
+    }),
 });
